7a94e1a6624690da188fff8c37a73772977c2fe9
sandip <sandip@gmail.com>
2024-12-07 11:31:27 -0800

    windows

--- lua/option.lua
+++ lua/option.lua
@@ -1,8 +1,8 @@
 local o = vim.o
 local g = vim.g
 local opt = vim.opt
+local uv = vim.loop
 
--- General Settings
 o.expandtab = true
 o.shiftwidth = 4
 o.tabstop = 4
@@ -11,30 +11,33 @@ o.smartindent = true
 o.numberwidth = 5
 o.ruler = false
 
--- Line Numbers
 o.nu = true
 o.rnu = true
 
--- Colors
 o.termguicolors = true
 
--- Undo Settings
-local undodir = os.getenv("HOME") .. "/.nvim/undodir"  -- Change to USERPROFILE for Windows
-os.execute("mkdir -p " .. undodir)  -- Ensure the directory exists
+local undodir
+if vim.loop.os_uname().sysname == "Linux" then
+    undodir = os.getenv("HOME") .. "/.nvim/undodir"
+elseif vim.loop.os_uname().sysname == "Windows_NT" then
+    undodir = os.getenv("USERPROFILE") .. "\\AppData\\Local\\nvim\\undodir"
+else
+    undodir = os.getenv("HOME") .. "/.nvim/undodir"
+end
+
+os.execute("mkdir -p " .. undodir)
+
 o.undodir = undodir
 o.undofile = true
 o.backup = false
 o.swapfile = false
 
--- Short messages
 opt.shortmess:append("sI")
 
--- Disable some default providers
 g.loaded_node_provider = 0
 g.loaded_python3_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
 
--- Smart Indentation
 opt.smartindent = true
 
